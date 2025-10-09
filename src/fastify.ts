80d484a50dd695b4e078a187a426324faabb69fb
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-10-09 17:04:06 -0300

    feat: rotas do usuário registrada

--- src/fastify.ts
+++ src/fastify.ts
@@ -1,8 +1,10 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { rotas_usuario } from "./routes/usuario.router.js";
 
 const fastify = Fastify({ logger: true });
 
 fastify.register(cors, { origin: "*" });
+fastify.register(rotas_usuario, { prefix: "/usuarios"});
 
 export default fastify;
