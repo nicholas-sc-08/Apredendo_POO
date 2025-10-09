521db0b014e78f28b35bf8ea4380155b44658976
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-10-09 15:25:32 -0300

    feat: interface usuario

---
+++ src/types/I_usuario.types.ts
@@ -0,0 +1,21 @@
+export interface I_usuario {
+
+    id: number;
+    nome: string;
+    email: string;
+    senha: string;
+};
+
+export interface I_create_usuario {
+
+    nome: string;
+    email: string;
+    senha: string;
+};
+
+export interface I_update_usuario {
+
+    nome?: string;
+    email?: string;
+    senha?: string;
+};
